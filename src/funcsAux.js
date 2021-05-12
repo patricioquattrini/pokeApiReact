567fdb49248a29e3852b464250076baa619a447a
patricioquattrini <58832743+patricioquattrini@users.noreply.github.com>
2021-05-12 17:30:36 -0300

    codifo un poco mas limpio

--- src/funcsAux.js
+++ src/funcsAux.js
@@ -1,32 +1,21 @@
 export const hacer10Fetch = async (imgUrl, x) =>{
     
-    let datass = [];
-    let tupla = [];
-    let fs = [];
+    
+    let promesasFetch = [];
     
     for(let j = 1; j < 11; j++){    
         let id = j + x;
-        console.log("que mierda es esto"+id)
-        let f = fetch("https://pokeapi.co/api/v2/pokemon/"+id)
-        fs.push(f)        
+        let f = fetch("https://pokeapi.co/api/v2/pokemon/"+id);
+        promesasFetch.push(f);        
     }
     
-    return Promise.all(fs)
-                .then(res => {
-                    let js = res.map(r =>r.json())
-                    return Promise.all(js).then(es =>{ 
-
-                        es.forEach((e)=>{
-                            tupla = [e, imgUrl+padLeft(e.id,3)+".png"];
-                            datass.push(tupla)
-                        })
-                        console.log(datass)
-                        return datass;
-                    })
-                   
+    return Promise.all(promesasFetch)
+                .then(resultadosFetch => {
+                    let promesasJson = resultadosFetch.map(r => r.json());
+                    return Promise.all(promesasJson)
+                        .then(resultadoJason => resultadoJason.map(e => [e, imgUrl+padLeft(e.id,3)+".png"]));
                 })
-                            
-                .catch(e => console.log("algo salio mal en las promesas:"+e))
+                .catch(e => console.log("Algo salio mal en las promesas:"+e))
 };
 
 function padLeft(value, length) {
